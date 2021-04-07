d98225cb2b8bda1c720d9831cf47ecb25be4f35e
Ashutosh Kushawaha <akushawaha@qkly.io>
2021-04-07 21:17:56 +0545

    project setup done

---
+++ ReviewCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReviewCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ReviewCsharp/Test/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewCsharp.Test
+{
+    public class TestClass
+    {
+        public void Calculate()
+        {
+        }
+    }
+}
